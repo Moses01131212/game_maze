8fb158a21abbb5f933723cbdff5dc8cf43d0dc65
Moses01131212 <113287809+Moses01131212@users.noreply.github.com>
2022-10-08 15:27:51 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,60 @@
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    info.changeLifeBy(-1)
+    pause(100)
+    tiles.placeOnRandomTile(mySprite, assets.tile`我的貼圖`)
+    pause(2000)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖2`, function (sprite, location) {
+    game.over(true, effects.starField)
+})
+controller.player2.onEvent(ControllerEvent.Connected, function () {
+    mySprite2 = sprites.create(img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . c d f d d f d e e f f . . . . 
+        . c d f d d f d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c d d d d c d d e e b d c . f f 
+        c c c c c d d d e e f c . f e f 
+        . f d d d d d e e f f . . f e f 
+        . . f f f f f e e e e f . f e f 
+        . . . . f e e e e e e e f f e f 
+        . . . f e f f e f e e e e f f . 
+        . . . f e f f e f e e e e f . . 
+        . . . f d b f d b f f e f . . . 
+        . . . f d d c d d b b d f . . . 
+        . . . . f f f f f f f f f . . . 
+        `, SpriteKind.Player)
+    controller.player2.moveSprite(mySprite2)
+    info.player2.setLife(3)
+    tiles.placeOnRandomTile(mySprite2, assets.tile`我的貼圖`)
+})
+info.onLifeZero(function () {
+    game.over(false, effects.blizzard)
+})
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . b 5 5 b . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . b b b b b 5 5 5 5 5 5 5 b . . 
+    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+    . . b d 5 5 b 1 f f 5 4 4 c . . 
+    b b d b 5 5 5 d f b 4 4 4 4 b . 
+    b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`層級4`)
+tiles.placeOnRandomTile(mySprite, assets.tile`我的貼圖`)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
+info.setLife(3)
